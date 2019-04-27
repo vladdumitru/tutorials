e86ce6c61849237f838b7de0f0685d2952932345
vladdumitru <vlad_dumitru@yahoo.com>
2019-04-27 21:32:45 +0300

    Abstract Factory design pattern - minor change

--- src/main/java/com/vlad/designpatterns/creational/abstractfactory/FactoryProvider.java
+++ src/main/java/com/vlad/designpatterns/creational/abstractfactory/FactoryProvider.java
@@ -3,7 +3,7 @@ package com.vlad.designpatterns.creational.abstractfactory;
 public class FactoryProvider {
     public static AbstractFactory getFactory(String choice){
         
-        if("Animal".equalsIgnoreCase(choice)){
+        if("Toy".equalsIgnoreCase(choice)){
             return new AnimalFactory();
         }
         else if("Color".equalsIgnoreCase(choice)){
