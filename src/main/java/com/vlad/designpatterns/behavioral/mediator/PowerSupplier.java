ebab678299b6dbbb9e57cde7cda5a33c351d42d2
vladdumitru <vlad_dumitru@yahoo.com>
2019-04-28 15:58:56 +0300

    Mediator pattern

---
+++ src/main/java/com/vlad/designpatterns/behavioral/mediator/PowerSupplier.java
@@ -0,0 +1,11 @@
+package com.vlad.designpatterns.behavioral.mediator;
+
+public class PowerSupplier {
+    public void turnOn() {
+        // implementation
+    }
+
+    public void turnOff() {
+        // implementation
+    }
+}
